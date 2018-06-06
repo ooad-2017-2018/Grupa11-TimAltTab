93d9ac8693f55bc492085f43b80fbcbbdf5427bd
fzukorlic1 <fzukorlic1@etf.unsa.ba>
2018-06-07 00:10:07 +0200

    refaktoring(vol 2)

--- Projekat/BeFit/BeFit/Adapter.cs
+++ Projekat/BeFit/BeFit/Adapter.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace BeFit
 {
-	class Adapter
+	interface IKlijentStatistika
 	{
+		double ProsjecnaCijenaKupljenogPrograma();
+		double UkupnoPotrosenoNovcaNaPrograme();
+		int BrojKupljenihPrograma();
+
+	}
+	class AdapterKlijent:Klijent, IKlijentStatistika
+	{
+		public int BrojKupljenihPrograma()
+		{
+			return this.KupljeniProgrami.Count;
+		}
+		public double UkupnoPotrosenoNovcaNaPrograme()
+		{
+			double svota = 0;
+			foreach(var i in this.KupljeniProgrami)
+			{
+				svota += i.Cijena;
+			}
+			return svota;
+		}
+		public double ProsjecnaCijenaKupljenogPrograma()
+		{
+			return UkupnoPotrosenoNovcaNaPrograme() / BrojKupljenihPrograma();
+		}
 	}
 }
